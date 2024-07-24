533f13f94a81aff55326877c73555975cbda0f51
SAVVY1710 <163214365+SAVVY1710@users.noreply.github.com>
2024-07-24 01:33:31 -0700

    added JComponents

--- MyFirstCardLayout.java
+++ MyFirstCardLayout.java
@@ -5,12 +5,6 @@ MyFirstCardLayout.java
 Use CardLayout
  */
 
-import java.awt.BorderLayout;
-import java.awt.CardLayout;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -55,6 +49,72 @@ public class MyFirstCardLayout {
             JPanel btnPanel3 = new JPanel(new GridLayout(2,1));
             JPanel btnPanel4 = new JPanel(new GridLayout(2,1));
 
+            JPanel btnpanel1 = new JPanel(new FlowLayout(FlowLayout.CENTER));
+            JPanel btnpanel2 = new JPanel(new FlowLayout(FlowLayout.CENTER));
+            JPanel btnpanel3 = new JPanel(new FlowLayout(FlowLayout.CENTER));
+            JPanel btnpanel4 = new JPanel(new FlowLayout(FlowLayout.CENTER));
+            JPanel btnpanel5 = new JPanel(new FlowLayout(FlowLayout.CENTER));
+            JPanel btnpanel6 = new JPanel(new FlowLayout(FlowLayout.CENTER));
+            JPanel btnpanel7 = new JPanel(new FlowLayout(FlowLayout.CENTER));
+            JPanel btnpanel8 = new JPanel(new FlowLayout(FlowLayout.CENTER));
+
+            btnPanel.add(btnpanel1);
+            btnPanel.add(btnpanel2);
+            btnPanel2.add(btnpanel3);
+            btnPanel2.add(btnpanel4);
+            btnPanel3.add(btnpanel5);
+            btnPanel3.add(btnpanel6);
+            btnPanel4.add(btnpanel7);
+            btnPanel4.add(btnpanel8);
+
+            Color blue1 = new Color(3,173,252);
+            JPanel blanc = new JPanel();
+            JPanel blank = new JPanel(new FlowLayout(FlowLayout.CENTER));
+            JPanel blank1 = new JPanel();
+            blank.setPreferredSize(new Dimension(50, 300));
+            blanc.setPreferredSize(new Dimension(600, 150));
+            blank1.setPreferredSize(new Dimension(50, 300));
+            JTextArea blue = new JTextArea("\n\n\n\n\n\n\n\n                                                                            Button1");
+            JTextArea red = new JTextArea("\n\n\n\n\n\n\n\n                                                                            Button2");
+            JTextArea green = new JTextArea("\n\n\n\n\n\n\n\n                                                                            Button3");
+            JTextArea yellow = new JTextArea("\n\n\n\n\n\n\n\n                                                                            Button4");
+            blue.setBackground(blue1);
+            red.setBackground(Color.red);
+            green.setBackground(Color.green);
+            yellow.setBackground(Color.yellow);
+            blue.setPreferredSize(new Dimension(500,300));
+            panel1.add(blue, BorderLayout.CENTER);
+            panel2.add(red, BorderLayout.CENTER);
+            panel3.add(green, BorderLayout.CENTER);
+            panel4.add(yellow, BorderLayout.CENTER);
+
+            JButton btn1 = new JButton("Display Card 1");
+            JButton btn2 = new JButton("Display Card 2");
+            JButton btn3 = new JButton("Display Card 3");
+            JButton btn4 = new JButton("Display Card 4");
+            JButton next = new JButton("Next");
+            JButton previous = new JButton("Previous");
+
+            JButton btn11 = new JButton("Display Card 1");
+            JButton btn21 = new JButton("Display Card 2");
+            JButton btn31 = new JButton("Display Card 3");
+            JButton btn41 = new JButton("Display Card 4");
+            JButton next1 = new JButton("Next");
+            JButton previous1 = new JButton("Previous");
+
+            JButton btn12 = new JButton("Display Card 1");
+            JButton btn22 = new JButton("Display Card 2");
+            JButton btn32 = new JButton("Display Card 3");
+            JButton btn42 = new JButton("Display Card 4");
+            JButton next2 = new JButton("Next");
+            JButton previous2 = new JButton("Previous");
+
+            JButton btn13 = new JButton("Display Card 1");
+            JButton btn23 = new JButton("Display Card 2");
+            JButton btn33 = new JButton("Display Card 3");
+            JButton btn43 = new JButton("Display Card 4");
+            JButton next3 = new JButton("Next");
+            JButton previous3 = new JButton("Previous");
         }
         class Handler implements ActionListener{public void actionPerformed(ActionEvent e){}}
     }
